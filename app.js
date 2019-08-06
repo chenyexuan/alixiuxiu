e9911614bf480e6b8d2c2b0cc6ce5f04650b6743
yexuan <158521138@qq.com>
2019-08-06 08:14:05 +0800

    添加导航守卫

--- app.js
+++ app.js
@@ -12,7 +12,26 @@ app.use(session({
     secret:'my_albx_35',
     resave:false,
     saveUninitialized:false
-}))
+}));
+app.use(function(req,res,next){
+    if(req.session.isLogin && req.session.isLogin=='true'||req.url=='/admin/login'||req.url.indexOf('admin/')== -1){
+        next();
+    } else  {
+        res.redirect('/admin/login');
+    }
+})
+// app.use(function (req, res, next) {
+//     // 三种场合不用登陆
+//     // 1.登陆页
+//     // 2.前面三个页面不用登陆
+//     // 3.有登陆状态
+//     if (req.session.isLogin && req.session.isLogin == 'true' || req.url == '/admin/login' || req.url.indexOf('/admin') == -1) {
+//         next()
+//     } else {
+//         // redirect:实现重定向
+//        res.redirect('/admin/login')
+//     } 
+// })
 app.use('/assets',express.static('assets'));
 app.use('/uploads', express.static('uploads'));
 app.set('view engine','ejs');
