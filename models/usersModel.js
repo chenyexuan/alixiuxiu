24305aac4831b714c8fbf0f006f336bb86d88109
yexuan <158521138@qq.com>
2019-08-09 08:26:18 +0800

    修改

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,9 +1,10 @@
 const con = require('../utils/myconn');
+
 exports.testEmail=function(email,callback){
     // console.log(email);
     let sql = `SELECT * FROM users where \`email\`= '${email}'`;
     // console.log(sql);
-    conn.query(sql,(err,results)=>{
+    con.query(sql,(err,results)=>{
         console.log(3);
         if(err) {
             console.log(2);
